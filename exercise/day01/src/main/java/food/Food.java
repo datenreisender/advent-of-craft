f414652f5880758295d26bfcef382e36766ebc3f
Marko Schulz <Marko.Schulz@nordicsemi.no>
2023-12-01 13:15:07 +0100

    Do day 1

--- exercise/day01/src/main/java/food/Food.java
+++ exercise/day01/src/main/java/food/Food.java
@@ -5,15 +5,11 @@ import java.util.UUID;
 import java.util.function.Supplier;
 
 public record Food(LocalDate expirationDate,
-                   Boolean approvedForConsumption,
-                   UUID inspectorId) {
+        Boolean approvedForConsumption,
+        UUID inspectorId) {
     public boolean isEdible(Supplier<LocalDate> now) {
-        if (this.expirationDate.isAfter(now.get()) &&
+        return this.expirationDate.isAfter(now.get()) &&
                 this.approvedForConsumption &&
-                this.inspectorId != null) {
-            return true;
-        } else {
-            return false;
-        }
+                this.inspectorId != null;
     }
 }
