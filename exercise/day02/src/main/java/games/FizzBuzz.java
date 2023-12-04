e8ac122110932d283df64e008d582eb192ab059c
Marko Schulz <Marko.Schulz@nordicsemi.no>
2023-12-04 10:05:03 +0100

    Do day 2

--- exercise/day02/src/main/java/games/FizzBuzz.java
+++ exercise/day02/src/main/java/games/FizzBuzz.java
@@ -3,25 +3,23 @@ package games;
 public class FizzBuzz {
     private FizzBuzz() {
     }
-    
+
+    public static boolean valid(Integer input) {
+        return input > 0 && input <= 100;
+    }
+
     public static String convert(Integer input) throws OutOfRangeException {
-        if (input > 0) {
-            if (input <= 100) {
-                if (input % 3 == 0 && input % 5 == 0) {
-                    return "FizzBuzz";
-                }
-                if (input % 3 == 0) {
-                    return "Fizz";
-                }
-                if (input % 5 == 0) {
-                    return "Buzz";
-                }
-                return input.toString();
-            } else {
-                throw new OutOfRangeException();
-            }
-        } else {
-            throw new OutOfRangeException();
+        if (!valid(input)) throw new OutOfRangeException();
+
+        if (input % 3 == 0 && input % 5 == 0) {
+            return "FizzBuzz";
         }
+        if (input % 3 == 0) {
+            return "Fizz";
+        }
+        if (input % 5 == 0) {
+            return "Buzz";
+        }
+        return input.toString();
     }
 }
